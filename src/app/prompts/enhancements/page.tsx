fd2ee0fb5a1f5d94b41b4852f8ac722ba9d49b74
DanSias <dan.sias@gmail.com>
2025-02-08 21:33:24 -0500

    Prompt Enhancements Page

---
+++ src/app/prompts/enhancements/page.tsx
@@ -0,0 +1,9 @@
+import PromptEnhancements from "@/components/PromptEnhancements";
+
+export default function EnhancementsPage() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-gray-200 p-6">
+      <PromptEnhancements />
+    </div>
+  );
+}
