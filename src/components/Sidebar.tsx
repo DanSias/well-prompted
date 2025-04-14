5d2300c0391a39a499be5065520f54085dcd97b6
DanSias <dan.sias@gmail.com>
2025-04-14 00:02:30 -0400

    Hide sidebar on mobile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,41 +13,44 @@
  * Usage:
  * - Displayed on the left side of the prompt generation pages for easy navigation between different prompt types.
  */
-
 "use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+// import { useState } from "react";
+// import { FaChevronDown } from "react-icons/fa";
 import promptTypes from "@/constants/promptTypes";
 
 export default function Sidebar() {
   const pathname = usePathname();
+  // const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <div className="w-72 h-full min-h-screen bg-slate-800 text-white p-4 shadow-lg">
-      <h1 className="text-2xl font-bold mb-6 text-left mt-2 text-sky-50">
-        Well Prompted
-      </h1>
-      <nav className="space-y-4">
-        {promptTypes.map(({ name, path, icon: Icon, description }) => (
-          <div key={path} className="relative">
-            <Link href={path} className="block w-full group">
-              <div
-                className={`flex items-center w-full p-3 mb-2 rounded-lg cursor-pointer transition-colors hover:bg-gray-700 ${
-                  pathname === path ? "bg-gray-700" : ""
-                }`}>
-                <Icon className="text-xl mr-3" />
-                <span className="text-lg">{name}</span>
-
-                {/* Popover inside the Link for correct hover behavior */}
-                <div className="absolute top-1/2 left-full transform -translate-y-1/2 ml-2 w-64 bg-gray-800 text-gray-200 p-4 rounded-lg shadow-lg z-10 opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity duration-200">
-                  <p className="text-sm">{description}</p>
+    <>
+      {/* Full Sidebar for md+ screens */}
+      <div className="hidden md:block w-72 sticky top-0 h-screen bg-gray-800 text-white p-4 shadow-lg">
+        <h1 className="text-2xl font-bold mb-6 text-left mt-2 text-sky-50">
+          <Link href="/">Well Prompted</Link>
+        </h1>
+        <nav className="space-y-4">
+          {promptTypes.map(({ name, path, icon: Icon, description }) => (
+            <div key={path} className="relative">
+              <Link href={path} className="block w-full group">
+                <div
+                  className={`flex items-center w-full p-3 mb-2 rounded-lg cursor-pointer transition-colors hover:bg-gray-700 ${
+                    pathname === path ? "bg-gray-700" : ""
+                  }`}>
+                  <Icon className="text-xl mr-3" />
+                  <span className="text-lg">{name}</span>
+                  <div className="absolute top-1/2 left-full transform -translate-y-1/2 ml-2 w-64 bg-gray-800 text-gray-200 p-4 rounded-lg shadow-lg z-10 opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity duration-200">
+                    <p className="text-sm">{description}</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </nav>
-    </div>
+              </Link>
+            </div>
+          ))}
+        </nav>
+      </div>
+    </>
   );
 }
